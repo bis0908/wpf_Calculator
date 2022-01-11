e7c93dd183fffc221ead4f72e6f2f58ec65b3dc6
Wonseok Kong <suckiejjang@naver.com>
2022-01-11 15:59:14 +0900

    Clear 기능 추가 (2022.01.11)

--- wpf_Calculator/MainWindow.xaml.cs
+++ wpf_Calculator/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ namespace wpf_Calculator
         private bool newButton;     // 새로 숫자가 시작되어야 하는 것을 알려주는 flag
         private char myOperator;    // 현재 계산할 Operator
         private double savedValue;  // 4칙 연산자를 누르면 txtResult 값을 저장
-
+        private double num = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -53,6 +53,18 @@ namespace wpf_Calculator
             if (myOperator == '+')
             {
                 txtResult.Text = (savedValue + double.Parse(txtResult.Text)).ToString();
+            }
+        }
+
+        // 'C' 버튼을 누르면 계산 초기화
+        private void btnClr_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            myOperator = btn.Content.ToString()[0];
+            if (myOperator == 'C')
+            {
+                txtResult.Text = "0";
+                savedValue = 0;
             }
         }
     }
