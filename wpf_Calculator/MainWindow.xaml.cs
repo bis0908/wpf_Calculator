9168de8e890d7e889ce4bf08a9ce2737e1eeb6e2
Wonseok Kong <suckiejjang@naver.com>
2022-01-11 15:10:43 +0900

    소스 코드 입력

--- wpf_Calculator/MainWindow.xaml.cs
+++ wpf_Calculator/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace wpf_Calculator
 {
@@ -20,9 +8,49 @@ namespace wpf_Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+        // 숫자 버튼의 처리
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            string number = btn.Content.ToString();
+            if (txtResult.Text == "0" || newButton == true)
+            {
+                txtResult.Text = number;
+                newButton = false;
+            }
+            else
+            {
+                txtResult.Text = txtResult.Text + number;
+            }
+
+        }
+
+        private void btnOp_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            saveValue = double.Parse(txtResult.Text);
+            myOperator = btn.Content.ToString()[0];
+            newButton = true;
+        }
+
+        private void Dot_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtResult.Text.Contains(".") == false)
+                txtResult.Text += ".";
+        }
+
+        private void Equal_Click(object sender, RoutedEventArgs e)
+        {
+            if (myOperator == '+')
+            {
+                txtResult.Text = (savedValue + double.Parse(txtResult.Text)).ToString();
+            }
         }
     }
 }
